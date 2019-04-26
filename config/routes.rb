9b32d5eeff935a58e9cb7e3626f09e7f543d2984
github username <github email address>
2019-04-26 15:03:12 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: [:index, :new, :create]
-  get '/student/:id', to: 'students#show', as: 'student'
+  resources :students, only: [:index, :new, :show, :create]
+  # get '/student/:id', to: 'students#show', as: 'student'
+  # rails console will not run unless the above was commented out
 end
